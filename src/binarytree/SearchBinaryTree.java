d5102d409702573b90571e8e6cc7e1c18f427f61
dengxingda <hohoemi1993@gmail.com>
2019-05-05 13:50:17 +0800

    search b tree

---
+++ src/binarytree/SearchBinaryTree.java
@@ -0,0 +1,23 @@
+package binarytree;
+
+/**
+ * Created on 2019/3/25<br>
+ *
+ * @author dengxingda
+ * @version 1.0
+ */
+public class SearchBinaryTree {
+
+    // get
+    public boolean isContains(BinaryTreeNode root, int target, BinaryTreeNode node) {
+        if (null == root) {
+            return false;
+        }
+
+        return true;
+    }
+    // insert
+
+    // delete
+
+}
---
+++ src/lc021tolc040/lc025/Solution.java
@@ -0,0 +1,11 @@
+package lc021tolc040.lc025;
+
+/**
+ * Created on 2019/4/10<br>
+ *
+ * @author dengxingda
+ * @version 1.0
+ */
+public class Solution {
+
+}
