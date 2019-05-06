829508330e7f0cb1de58e0df96cf8f70cc9ea596
dengxingda <hohoemi1993@gmail.com>
2019-05-06 13:26:38 +0800

    26 27

---
+++ src/lc021tolc040/lc026/RemoveDuplicates.java
@@ -0,0 +1,22 @@
+package lc021tolc040.lc026;
+
+/**
+ * Created on 2019/5/5<br>
+ *
+ * @author dengxingda
+ * @version 1.0
+ */
+public class RemoveDuplicates {
+
+    public int removeDuplicates(int[] nums) {
+        if (nums.length <= 1) return nums.length;
+        int index = 0;
+        for(int i=1; i<nums.length; i++) {
+            if (nums[index] != nums[i]) {
+                index++;
+                nums[index] = nums[i];
+            }
+        }
+        return index + 1;
+    }
+}
---
+++ src/lc021tolc040/lc027/RemoveElement.java
@@ -0,0 +1,27 @@
+package lc021tolc040.lc027;
+
+/**
+ * Created on 2019/5/6<br>
+ * Given an array nums and a value val, remove all instances of that value in-place and return the new length
+ *
+ * @author dengxingda
+ * @version 1.0
+ */
+public class RemoveElement {
+
+    public int removeElement(int[] nums, int val) {
+        int j = nums.length - 1;
+        int i = 0;
+        while (i<=j) {
+            if (nums[i] == val) {
+                nums[i] = nums[j];
+                j--;
+            }
+            else {
+                i++;
+            }
+        }
+
+        return i;
+    }
+}
