5f5aa3e8020a7de08eda56c063cf92d781c3b71f
dengxingda <hohoemi1993@gmail.com>
2019-05-07 14:40:57 +0800

    25

---
+++ src/common/ListNode.java
@@ -0,0 +1,13 @@
+package common;
+
+/**
+ * Created on 2019/5/7<br>
+ *
+ * @author dengxingda
+ * @version 1.0
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int x) { val = x; }
+}
--- src/lc021tolc040/lc025/Solution.java
+++ src/lc021tolc040/lc025/Solution.java
@@ -1,5 +1,7 @@
 package lc021tolc040.lc025;
 
+import common.ListNode;
+
 /**
  * Created on 2019/4/10<br>
  *
@@ -7,5 +9,35 @@ package lc021tolc040.lc025;
  * @version 1.0
  */
 public class Solution {
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (null == head || k == 1) return head;
+
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode pre = dummy;
+        ListNode cur = dummy;
+        ListNode next = dummy;
+        int length = 0;
+        while (cur.next != null) {
+            cur =  cur.next;
+            length++;
+        }
+
+        while (length >= k) {
+            cur = pre.next;
+            next = cur.next;
+            for (int i=1; i<k; i++) {
+                cur.next = next.next;
+                next.next = pre.next;
+                pre.next = next;
+                next = cur.next;
+            }
+            pre = cur;
+            length -= k;
+        }
+
+        return dummy.next;
+    }
 
 }
