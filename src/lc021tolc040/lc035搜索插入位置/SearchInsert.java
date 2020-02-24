8d89b00d7139976f5203e4c481757c1a8e8df894
dxd <hohoemi1993@gmail.com>
2020-02-24 12:01:44 +0800

    二分查找模板

---
+++ src/lc021tolc040/lc035搜索插入位置/SearchInsert.java
@@ -0,0 +1,26 @@
+package lc021tolc040.lc035;
+
+/**
+ * Created on 2019/5/9<br>
+ *
+ * @author dengxingda
+ * @version 1.0
+ */
+public class SearchInsert {
+    // 1，3，5，6   2
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            } else if(nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
